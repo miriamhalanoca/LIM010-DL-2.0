50040837daad37db8c6f8a3d727a7d445621b780
miriamhalanoca <miriamhalanoca15@gmail.com>
2019-08-22 13:17:03 -0500

    mejoras del diseño en css

--- src/data.js
+++ src/data.js
@@ -8,11 +8,3 @@ const example = () => {
 };
 
 window.example = example;
-// if (typeof datos[i].yearOfBirth === 'number') {
-    //   all +=
-    //     `<div><p>? Edad</br><strong>${2019 - datos[i].yearOfBirth}</strong></p></div>`;
-    // } else {
-    //   all += `<p>No tiene fecha de nacimiento</p>`;
-    // }
-    // all += `<p>? Actor</br><strong>${datos[i].actor}</strong></p></div>
-    //   </div>`;
--- src/main.js
+++ src/main.js
@@ -1,21 +1,38 @@
 /* Manejo del DOM */
-/* Manejo del DOM */
-const data = document.getElementById('data');
-const data1 = (fetch('https://raw.githubusercontent.com/miriamhalanoca/LIM010-DL-2.0/master/src/data/potter.json')
-.then(response => response.json()));
-Promise.all(data1)
-.then(resultado => {
-  console.log(resultado)
-  const urlspeli = (data) => {
-    let almacenar = '';
-    for (data in Object) {
-      let item = `
-        
-        
-           `
-      almacenar += item;
+const data = document.getElementById("data");
+
+const dataJSON = () => {
+  fetch('https://raw.githubusercontent.com/Laboratoria/LIM009-DL-2.0/master/src/data/potter.json')
+    .then(res => res.json())
+    .then(datos => {
+      paintAll(datos);
+    });
+};
+dataJSON();
+
+
+const paintAll = (datos) => {
+  let all = '';
+  for (let i = 0; i < datos.length; i++) {
+    all += `
+        <div class="cards">
+        <figure class="cards-img">
+        <img src= ${datos[i].image} />
+        </figure>
+        <div class="text"><p><strong>${datos[i].name}</strong></p>
+        <p> ${datos[i].house ? datos[i].house : 'No tiene casa'}</p></div>
+        <div class="texto">
+        <p>? Especie</br><strong>${datos[i].species}</strong></p>
+        <p>? Rol</br><strong>${datos[i].hogwartsStudent ? 'Estudiante' : 'Staff'}</strong></p>
+        `;
+  if (typeof datos[i].yearOfBirth === 'number') {
+      all +=
+        `<div><p>? Edad</br><strong>${2019 - datos[i].yearOfBirth}</strong></p></div>`;
+    } else {
+      all += `<p>No tiene fecha de nacimiento</p>`;
     }
-    return almacenar;
-  };
-  elemt.innerHTML = urlspeli(resultado);
-});
+    all += `<p>? Actor</br><strong>${datos[i].actor}</strong></p></div>
+      </div>`;
+  }
+  data.innerHTML = all;
+};
