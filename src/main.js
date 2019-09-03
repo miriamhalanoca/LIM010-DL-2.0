fdcc37f7bf2c409964d1f31f3ff894893e4d1112
miriamhalanoca <miriamhalanoca15@gmail.com>
2019-09-03 18:35:19 -0500

    subiendo cambios que se hizo en javascript

--- src/main.js
+++ src/main.js
@@ -1,38 +1,68 @@
 /* Manejo del DOM */
 const data = document.getElementById("data");
+const botonAscendente= document.getElementById("ASCENDENTE");
 
-const dataJSON = () => {
+
+
+const dataAlmacenados = () => {
   fetch('https://raw.githubusercontent.com/Laboratoria/LIM009-DL-2.0/master/src/data/potter.json')
     .then(res => res.json())
-    .then(datos => {
-      paintAll(datos);
+    .then(resultado => {
+      mostrar(resultado);
+
+      console.log(resultado);
     });
+   
 };
-dataJSON();
+dataAlmacenados();
 
-
-const paintAll = (datos) => {
-  let all = '';
+const mostrar = (datos) => {
+  let almacenar = '';
   for (let i = 0; i < datos.length; i++) {
-    all += `
+    almacenar += `
         <div class="cards">
         <figure class="cards-img">
         <img src= ${datos[i].image} />
         </figure>
-        <div class="text"><p><strong>${datos[i].name}</strong></p>
+        <div class="text"><p>${datos[i].name}</p>
         <p> ${datos[i].house ? datos[i].house : 'No tiene casa'}</p></div>
         <div class="texto">
-        <p>? Especie</br><strong>${datos[i].species}</strong></p>
-        <p>? Rol</br><strong>${datos[i].hogwartsStudent ? 'Estudiante' : 'Staff'}</strong></p>
+        <p>Especie</br>${datos[i].species}</p>
+        <p> Rol</br>${datos[i].hogwartsStudent ? 'Estudiante' : 'Staff'}</p>
         `;
   if (typeof datos[i].yearOfBirth === 'number') {
-      all +=
-        `<div><p>? Edad</br><strong>${2019 - datos[i].yearOfBirth}</strong></p></div>`;
+      almacenar +=
+        `<div><p> Edad</br>${2019 - datos[i].yearOfBirth}</p></div>`;
     } else {
-      all += `<p>No tiene fecha de nacimiento</p>`;
+      almacenar += `<p>No tiene fecha de nacimiento</p>`;
     }
-    all += `<p>? Actor</br><strong>${datos[i].actor}</strong></p></div>
+    almacenar += `<p> Actor</br>${datos[i].actor}</p></div>
       </div>`;
   }
-  data.innerHTML = all;
+  data.innerHTML = almacenar;
+  // console.log(datos);
 };
+
+const ascendente = (datos) => datos.sort((a, b) => {
+  if (a.yearOfBirth > b.yearOfBirth) {
+    return 1;
+  }
+  else {
+
+    return -1;
+  }
+});
+// const buscar = document.getElementById('buscar');
+// buscar.addEventListener('input', event => {
+//   const peliculasBuscada = busco (results.event.target.value.toLowerCase());
+//   pelicontenedor.innerHTML = urlspeli(peliculasBuscada);
+// });
+
+ botonAscendente.addEventListener('click', () => {
+const ordenar1 = ascendente(resultado);
+data.innerHTML=  mostrar(ordenar1);
+
+});
+
+window.mostrar = mostrar;
+window.ascendente = ascendente;
